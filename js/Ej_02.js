bd7d1e600fcacd247c496219dd152bdb2717071e
Anto Ortega <antonellaportega@gmail.com>
2023-11-09 13:08:11 -0300

    ejercicio 2  terminado

--- js/Ej_02.js
+++ js/Ej_02.js
@@ -6,7 +6,8 @@
 // Escribe por pantalla el elemento que ocupa la segunda posición.
 // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
-const ciudades = []
+// carga de ciudades
+const ciudades = [];
 
 while (true) {
     const ciudad = prompt("Ingrese el nombre de una ciudad (o presione Cancelar para finalizar):");
@@ -21,9 +22,32 @@ while (true) {
     }
 }
 
-document.write('<h2>Lista de ciudades</h2>')
-document.write('<ul>')
-for (let i=0; i<ciudades.length; i++){
-    document.write(`<li> ${ciudades[i]} </li>`)
+// Muestro las ciudades
+document.write('<h2>Lista de ciudades</h2>');
+document.write('<ul>');
+for (let i = 0; i < ciudades.length; i++) {
+    document.write(`<li>${ciudades[i]}</li>`);
 }
-document.write('</ul>')
+document.write('</ul>');
+
+// Mostrar la longitud del arreglo
+let longitud = ciudades.length;
+document.write("<p>La longitud del arreglo es de: " + longitud + "</p>");
+
+// Mostrar las posiciones
+document.write("<p>En la primera posición se encuentra: " + ciudades[0] + "</p>");
+document.write("<p>En la tercera posición se encuentra: " + ciudades[2] + "</p>");
+document.write("<p>En la última posición se encuentra: " + ciudades[longitud - 1] + "</p>");
+
+// Agregar en la última posición
+let nuevo = "París";
+ciudades.push(nuevo);
+document.write("<p>Arreglo después de agregar un elemento: " + ciudades + "</p>");
+
+// Segunda posición
+document.write("<p>En la segunda posición se encuentra: " + ciudades[1] + "</p>");
+
+// Cambiar en la segunda posición
+let segundo = "Barcelona";
+ciudades[1] = segundo;
+document.write("<p>Arreglo después de la sustitución: " + ciudades + "</p>");
